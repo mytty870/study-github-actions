060cc79ee8f952f7bfc354d421f32952b26685d3
github mytty870 <otani11ace@icloud.com>
2023-10-15 20:46:11 +0900

    add run-name in actions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import styled from "styled-components"
 
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: #28788e;
+  height: 70px;
+`
+
 const HeaderTitle = styled.h1`
   color: #ff6f00ad;
   font-size: 1.2rem;
+`
+
+const MenuWrapper = styled.nav`
+  padding-top: 20px;
+`
+const Menu = styled.nav`
+  display: flex;
+  list-style: none;
+`
+
+const MenuItem = styled.li`
+  margin-right: 20px;
 `
 
 
 export const Header = () => {
   return (
     <>
-      <header>
+      <Wrapper>
         <HeaderTitle>なういBlog</HeaderTitle>
-      </header>
+        <MenuWrapper>
+          <Menu>
+            <MenuItem><a href="https://google.com">記事一覧</a></MenuItem>
+            <MenuItem>About</MenuItem>
+            <MenuItem>お問合せ</MenuItem>
+          </Menu>
+        </MenuWrapper>
+      </Wrapper>
     </>
   )
 }
--- src/tests/header.spec.ts
+++ src/tests/header.spec.ts
@@ -8,3 +8,7 @@ test('if you click count button once, the value increase by one', async({ page }
   const titleText = await page.innerText('h1')
   await expect(titleText).toBe('なういBlog')
 })
+
+// test('ヘッダーにある「記事一覧」をクリックすると、Googleのホームページに遷移する', async({page}) => {
+//   const 
+// })
